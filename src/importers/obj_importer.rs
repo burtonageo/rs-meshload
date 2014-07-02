23564b1ddfd065e564f4a19ce10329f50fc4cc22
George Burton <burtonageo@gmail.com>
2014-07-02 18:47:04 +0100

    Initialise color, points, normals, uvcoord & texture vectors

--- src/importers/obj_importer.rs
+++ src/importers/obj_importer.rs
@@ -18,21 +18,22 @@ impl<F0: Float, VC: VertexColor<F0>,
      M:  Mesh<F0, VC, F1, VP, F2, VN, F3, VU>> MeshImporter<F0, VC, F1, VP, F2, VN, F3, VU, M> for ObjImporter {
 
     fn load_mesh_file(&self, file_name: &str) -> IoResult<M> {
+        unimplemented!();
+
         let path = Path::new(file_name);
         let mut file = BufferedReader::new(try!(File::open(&path)));
 
-        let (mut temp_colors, mut final_colors): (&[VC], &[VC]);
-        let (mut temp_points, mut final_points): (&[VP], &[VP]);
-        let (mut temp_normals, mut final_normals): (&[VN], &[VN]);
-        let (mut temp_uvcoords, mut final_uvcoords): (&[VU], &[VU]);
+        let (mut temp_colors, mut final_colors): (&[VC], &[VC]) = (&[], &[]);
+        let (mut temp_points, mut final_points): (&[VP], &[VP]) = (&[], &[]);
+        let (mut temp_normals, mut final_normals): (&[VN], &[VN]) = (&[], &[]);
+        let (mut temp_uvcoords, mut final_uvcoords): (&[VU], &[VU]) = (&[], &[]);
 
-        let mut textures: &[&str];
+        let mut textures: &[&str] = &[];
 
         for l in file.lines().filter(|l| l.is_ok()) {
             let line = l.unwrap();
          }
 
-        unimplemented!();
         Ok(Mesh::new(None::<M>,
                      final_colors,
                      final_points,
