4ce5f96d537755086bc75a7f26f9124a812e4aa5
George Burton <burtonageo@gmail.com>
2014-07-13 21:47:50 +0100

    Updated types imported from tpyes crate

--- src/importers/mesh_importer.rs
+++ src/importers/mesh_importer.rs
@@ -1,10 +1,10 @@
 use std::io::{IoResult};
-use types::{Mesh, VertexColor, VertexPoint, VertexNormal, VertexUvCoord};
+use types::{Mesh, RgbaColor, Point, Normal, UvCoord};
 
-pub trait MeshImporter<F0: Float, VC: VertexColor<F0>,
-                       F1: Float, VP: VertexPoint<F1>,
-                       F2: Float, VN: VertexNormal<F2>,
-                       F3: Float, VU: VertexUvCoord<F3>,
+pub trait MeshImporter<F0: Float, VC: RgbaColor<F0>,
+                       F1: Float, VP: Point<F1>,
+                       F2: Float, VN: Normal<F2>,
+                       F3: Float, VU: UvCoord<F3>,
                        M:  Mesh<F0, VC, F1, VP, F2, VN, F3, VU>> {
     fn load_mesh_file(&self, file_name: &str) -> IoResult<M>;
 }
